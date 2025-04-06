efbcf38dc2f22535dc1a3235b706c5350549ed74
mc.jeong <root@14182>
2025-04-06 23:30:01 +0900

    supabase + auto 스크롤 리스트

--- lib/board/list.dart
+++ lib/board/list.dart
@@ -3,13 +3,13 @@ import 'package:indf_factory/supabase.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-typedef QueryBuilder<ItemType> = PostgrestTransformBuilder<List<ItemType>> Function(SupabaseClient client, int startRange, int endRange);
-typedef RenderBuilder<ItemType> = Widget Function(BuildContext context, ItemType item, int index);
+typedef QueryBuilder = Future<List<Map<String, dynamic>>> Function(SupabaseClient client, int startRange, int endRange);
+typedef RenderBuilder = Widget Function(BuildContext context, Map<String, dynamic> item, int index);
 
 
-class SupabaseAutoScrollListWidget<ItemType> extends StatefulWidget {
-  final QueryBuilder<ItemType> queryBuilder;
-  final RenderBuilder<ItemType> itemBuilder;
+class SupabaseAutoScrollListWidget extends StatefulWidget {
+  final QueryBuilder queryBuilder;
+  final RenderBuilder renderBuilder;
   final int pageSize;
 
   /*
@@ -18,31 +18,29 @@ class SupabaseAutoScrollListWidget<ItemType> extends StatefulWidget {
   const SupabaseAutoScrollListWidget({
     super.key,
     required this.queryBuilder,
-    required this.itemBuilder,
+    required this.renderBuilder,
     this.pageSize = 10,
   });
 
   @override
-  State<SupabaseAutoScrollListWidget<ItemType>> createState() => _AutoScrollWidgetState<ItemType>();
+  State<SupabaseAutoScrollListWidget> createState() => _AutoScrollWidgetState();
 }
 
-class _AutoScrollWidgetState<ItemType> extends State<SupabaseAutoScrollListWidget<ItemType>> {
+class _AutoScrollWidgetState extends State<SupabaseAutoScrollListWidget> {
   bool lastPage = false;
 
-  late final PagingController<int, ItemType> _pagingController = PagingController<int, ItemType>(
+  late final PagingController<int, Map<String, dynamic>> _pagingController = PagingController<int, Map<String, dynamic>>(
     getNextPageKey: (state) => lastPage ? null : (state.keys?.last ?? 0) + 1,
     fetchPage: fetchPage,
   );
 
 
-  Future<List<ItemType>> fetchPage(int pageKey) async {
+  Future<List<Map<String, dynamic>>> fetchPage(int pageKey) async {
     try {
       final client = SupabaseInstance().client;
       final startRange = (pageKey - 1) * widget.pageSize;
       final endRange = pageKey * widget.pageSize - 1;
       final newItems = await widget.queryBuilder(client, startRange, endRange);
-
-      // pageSize 보다 적다면 마지막 페이지 임
       lastPage = newItems.length < widget.pageSize;
       return newItems; // 새로운 방식에서는 데이터를 반환하면 자동 추가됨
     } catch (error) {
@@ -53,12 +51,12 @@ class _AutoScrollWidgetState<ItemType> extends State<SupabaseAutoScrollListWidge
   @override
   Widget build(BuildContext context) => PagingListener(
     controller: _pagingController,
-    builder: (context, state, fetchNextPage) => PagedListView<int, ItemType>(
+    builder: (context, state, fetchNextPage) => PagedListView<int, Map<String, dynamic>>(
       state: state,
       fetchNextPage: fetchNextPage,
       builderDelegate: PagedChildBuilderDelegate(
         itemBuilder: (context, item, index) {
-          return widget.itemBuilder(context, item, index);
+          return widget.renderBuilder(context, item, index);
         },
       ),
     ),
