399d86a77bda5c17a1471fcfc9b42936970034e4
mc.jeong <root@14182>
2025-04-18 18:22:10 +0900

    #1 마커 N개로 변경

--- lib/map/view.dart
+++ lib/map/view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'location.dart';
+import 'dart:math';
 
 class LocationViewWidget extends StatelessWidget {
-  final Marker marker;
+  final Set<Marker> marker;
   final Circle? circle;
   final double zoom;
   final bool myLocationButtonEnabled;
@@ -29,11 +30,45 @@ class LocationViewWidget extends StatelessWidget {
           ),
           myLocationButtonEnabled: myLocationButtonEnabled, // 현재 위치 버튼 활성화
           myLocationEnabled: myLocationEnabled, // 현재 위치 표시 활성화
-          markers: <Marker>{marker},
+          markers: _createMarker(marker),
           circles: <Circle>{_createCircle(location)},
         );
       },
     );
+  }
+
+  // 좌표가 같으면 노출이 안되기 때문에 같으면 약간 어긋난 좌표를 만들어 준다
+  Set<Marker> _createMarker(Set<Marker> originalMarkers) {
+    final Set<LatLng> duplicationCheck = {};
+    final Set<Marker> normalizeMarker = {};
+    for(final marker in originalMarkers) {
+      if (!duplicationCheck.contains(marker.position)) {
+        normalizeMarker.add(marker);
+        duplicationCheck.add(marker.position);
+        continue;
+      }
+      // 중복된 값이 존재함
+      while(true) {
+        final LatLng changedPosition = LatLng(
+          marker.position.latitude + (Random().nextDouble() / 10000),
+          marker.position.longitude + (Random().nextDouble() / 10000),
+        );
+        if (!duplicationCheck.contains(changedPosition)) {
+          normalizeMarker.add(Marker(
+            markerId: marker.markerId,
+            position: changedPosition,
+            infoWindow: InfoWindow(
+              title: marker.infoWindow.title,
+              snippet: marker.infoWindow.snippet,
+            ),
+            icon: marker.icon,
+          ));
+          duplicationCheck.add(changedPosition);
+          break;
+        }
+      }
+    }
+    return normalizeMarker;
   }
 
   Circle _createCircle(LatLng location) {
