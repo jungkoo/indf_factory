39add2b9426c6de814ccc7d9e56119f452d3f167
mc.jeong <root@14182>
2025-04-30 15:22:21 +0900

    #1 지도 이동 업데이트 로직 추가

--- lib/map/list.dart
+++ lib/map/list.dart
@@ -1,23 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:indf_factory/board/list.dart';
+import 'package:indf_factory/extensions/google_map.dart';
 import 'package:indf_factory/indf_location.dart';
-import 'package:indf_factory/map/location.dart';
-import 'package:indf_factory/map/view.dart';
-import '../indf_supabase.dart';
+import 'package:indf_factory/indf_supabase.dart';
 
 class SupabaseLocationListWidget extends StatefulWidget {
   final RenderBuilder renderBuilder;
   final MarkerBuilder markerBuilder;
   final QueryBuilder queryBuilder;
   final int pageSize;
+  final double zoom;
 
   const SupabaseLocationListWidget({
     super.key,
     required this.queryBuilder,
     required this.markerBuilder,
     required this.renderBuilder,
-    this.pageSize = 10
+    this.pageSize = 10,
+    this.zoom = 13.0
   });
 
   @override
@@ -26,37 +29,94 @@ class SupabaseLocationListWidget extends StatefulWidget {
 
 class _SupabaseLocationListWidgetState extends State<SupabaseLocationListWidget> {
   late QueryParameter queryParameter;
+  late LatLng _initialLocation;
+  late LatLng _mapLocation;
+  Timer? _debounceTimer; // 지도 이동 할때 잦은 호출을 막기 위한 방법
+
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    queryParameter = QueryParameter(
-      parameters: {},
-      pageNum: 1,
-      pageSize: widget.pageSize,
-    );
+    LocationInstance().currentLocation().then((location){
+      setState(() {
+        queryParameter = QueryParameter(
+          parameters: {},
+          pageNum: 1,
+          pageSize: widget.pageSize,
+          location: location,
+        );
+        _initialLocation = location;
+        _mapLocation = location;
+        _isLoading = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return LocationBuilder(
-      builder: (context, location) {
-        queryParameter.location = location;
-        return Stack(
-          alignment: AlignmentDirectional.topCenter,
-          children: [
-            _createMapWidget(location),
-            _createDraggableScrollableSheet(location),
-          ],
-        );
+    if (_isLoading) {
+      debugPrint("*** build : 로딩중");
+      return Center(child: CircularProgressIndicator());
+    }
+    return Stack(
+      alignment: AlignmentDirectional.topCenter,
+      children: [
+        _createMapWidget(),
+        _createDraggableScrollableSheet(),
+      ],
+    );
+  }
+
+
+  // 지도 그리기
+  Widget _createMapWidget() {
+    return FutureBuilder<List<Map<String, dynamic>>> (
+      future: searchMapData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          debugPrint('Error: ${snapshot.error}');
+          return Center(child: Text('오류가 발생했습니다.'));
+        } else if (snapshot.hasData) {
+          final List<Map<String, dynamic>> data = snapshot.data!;
+          return GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _mapLocation,
+              zoom: widget.zoom,
+            ),
+            myLocationButtonEnabled: true, // 현재 위치 버튼 활성화
+            myLocationEnabled: true, // 현재 위치 표시 활성화
+            markers: (data.map((item) => widget.markerBuilder(item)).whereType<Marker>().toSet()).deduplicate(),
+            circles: <Circle>{_initialLocation.currentCircle},
+            onCameraMove: (position) {
+              _debounceTimer?.cancel();
+              _debounceTimer = Timer(Duration(seconds: 1), () {
+                if (_isLoading) return;
+                if (!mounted) return;
+                if (position.target == queryParameter.location) return; // 위치가 같으면 업데이트 X
+
+                _isLoading = true;
+                setState(() {
+                  _mapLocation = position.target;
+                  queryParameter.location = _mapLocation;
+                  queryParameter.pageNum = 1;
+                });
+                _isLoading = false;
+              });
+            },
+          );
+        } else {
+          return Text("데이터 없음");
+        }
       },
     );
   }
 
-  Future<List<Map<String, dynamic>>> searchMapData(LatLng location) async {
+  Future<List<Map<String, dynamic>>> searchMapData() async {
     try {
       final client = SupabaseInstance().client;
-
       final dynamicList = await widget.queryBuilder(client, queryParameter);
       if (dynamicList.any((element) => element is! Map<String, dynamic>)) {
         throw Exception("queryResult 에 Map<String, dynamic> 타입이 아닌 요소가 포함되어 있습니다.");
@@ -68,34 +128,8 @@ class _SupabaseLocationListWidgetState extends State<SupabaseLocationListWidget>
     }
   }
 
-  // 지도 그리기
-  Widget _createMapWidget(LatLng location) {
-    return FutureBuilder<List<Map<String, dynamic>>> (
-      future: searchMapData(location),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          debugPrint('Error: ${snapshot.error}');
-          return Center(child: Text('오류가 발생했습니다.'));
-        } else if (snapshot.hasData) {
-          final List<Map<String, dynamic>> data = snapshot.data!;
-          // print("markers ==> ${data.map((item) => widget.markerBuilder(item)).whereType<Marker>().toSet()}");
-          return LocationViewWidget(
-            marker: data.map((item) => widget.markerBuilder(item)).whereType<Marker>().toSet(),
-            circle: null,
-            zoom: 13.0,
-            myLocationButtonEnabled: true, // 현재 위치 버튼 활성화
-            myLocationEnabled: true, // 현재 위치 표시 활성화
-          );
-        } else {
-          return Text("데이터 없음");
-        }
-      },
-    );
-  }
 
-  Widget _createDraggableScrollableSheet(LatLng location) {
+  Widget _createDraggableScrollableSheet() {
     return  DraggableScrollableSheet(
       initialChildSize: 0.3,
       minChildSize: 0.1,
@@ -116,7 +150,7 @@ class _SupabaseLocationListWidgetState extends State<SupabaseLocationListWidget>
                 margin: EdgeInsets.symmetric(vertical: 10),
                 decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(10)),
               ),
-              Expanded(child: _createAutoListWidget(scrollController, location)),
+              Expanded(child: _createAutoListWidget(scrollController)),
             ],
           ),
         );
@@ -124,8 +158,9 @@ class _SupabaseLocationListWidgetState extends State<SupabaseLocationListWidget>
     );
   }
 
-  Widget _createAutoListWidget(ScrollController scroller, LatLng location) {
+  Widget _createAutoListWidget(ScrollController scroller) {
     return SupabaseAutoScrollListWidget(
+        key: ValueKey(_mapLocation),
         queryBuilder: widget.queryBuilder,
         renderBuilder: widget.renderBuilder,
         queryParameter: queryParameter,
